665b24f072ad86cac88c212cdf67b9d829c872c7
Terence Yebuah <terence.yebuah@ashesi.edu.gh>
2024-07-04 13:52:17 +0000

    edited the event model

--- vlookup_v2/lib/models/event_model.dart
+++ vlookup_v2/lib/models/event_model.dart
@@ -1,4 +1,5 @@
 class Event {
+  final String event_id;
   final String title;
   final String email;
   final String description;
@@ -7,6 +8,7 @@ class Event {
   final String location;
 
   Event({
+    required this.event_id,
     required this.title,
     required this.email,
     required this.description,
@@ -18,6 +20,7 @@ class Event {
   // Factory constructor for instantiating a new Event from a map structure
   factory Event.fromJson(Map<String, dynamic> json) {
     return Event(
+      event_id: json['event_id'] as String,
       title: json['title'] as String,
       email: json['email'] as String,
       description: json['description'] as String,
@@ -30,6 +33,7 @@ class Event {
   // Converts a Event instance into a map
   Map<String, dynamic> toJson() {
     return {
+      'event_id': event_id,
       'title': title,
       'email': email,
       'description': description,
